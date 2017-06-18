51303207f5c08abcaf939a7bfa955e2ef770d26f
Phyo Kyaw <phyokyaw.uk@gmail.com>
2017-06-18 10:20:34 +0100

    Initial commit.
    Python http server to read values from bme280 chip.

---
+++ pi/server.py
@@ -0,0 +1,25 @@
+import SimpleHTTPServer
+import SocketServer
+import json
+import bme280
+
+PORT = 8090
+
+def readValueFromBme280():
+	temperature,pressure,humidity = bme280.readBME280All()
+	return json.dumps({'temperature': temperature, 'pressure': pressure, 'humidity': humidity})
+
+class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		if self.path == "/value":
+			self.send_response(200)
+			self.send_header('Content-Type', 'application/json')
+			self.end_headers()
+			self.wfile.write(readValueFromBme280())
+		else:
+			self.send_response(400, 'Bad Request: record does not exist')
+
+httpd = SocketServer.TCPServer(("", PORT), RequestHandler)
+
+print "Serving at port", PORT
+httpd.serve_forever()
