61b9d9e8c8c1e6575bb5c512714f5cb30de72844
Phyo Kyaw <phyokyaw.uk@gmail.com>
2017-06-18 10:20:34 +0100

    Added bme280.py.

--- pi/server.py
+++ pi/server.py
@@ -3,21 +3,23 @@ import SocketServer
 import json
 import bme280
 
+''' Uses https://bitbucket.org/MattHawkinsUK/rpispy-misc/raw/master/python/bme280.py '''
+
 PORT = 8090
 
 def readValueFromBme280():
-	temperature,pressure,humidity = bme280.readBME280All()
-	return json.dumps({'temperature': temperature, 'pressure': pressure, 'humidity': humidity})
+    temperature,pressure,humidity = bme280.readBME280All()
+    return json.dumps({'temperature': temperature, 'pressure': pressure, 'humidity': humidity})
 
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def do_GET(self):
-		if self.path == "/value":
-			self.send_response(200)
-			self.send_header('Content-Type', 'application/json')
-			self.end_headers()
-			self.wfile.write(readValueFromBme280())
-		else:
-			self.send_response(400, 'Bad Request: record does not exist')
+    def do_GET(self):
+        if self.path == "/value":
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(readValueFromBme280())
+        else:
+            self.send_response(400, 'Bad Request: record does not exist')
 
 httpd = SocketServer.TCPServer(("", PORT), RequestHandler)
 
